57b3c9e29506f61cf0f74153aa10c02f173e3c52
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-22 14:03:39 +0630

    class pelajar dengan member, konstruktor dan dekstruktor

--- ParadigmaOOP_143/ParadigmaOOP_143.cpp
+++ ParadigmaOOP_143/ParadigmaOOP_143.cpp
@@ -19,3 +19,20 @@ public:
 	}
 
 };
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar Dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar Dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+	}
+};
